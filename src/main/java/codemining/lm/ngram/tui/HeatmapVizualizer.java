37e531721699c7f8bd8c94532989f8e4072acbfc
Miltos Allamanis <m.allamanis@ed.ac.uk>
2014-03-19 17:46:40 +0000

    Created pretty printing by extracting old code.

--- src/main/java/codemining/lm/ngram/tui/HeatmapVizualizer.java
+++ src/main/java/codemining/lm/ngram/tui/HeatmapVizualizer.java
@@ -3,104 +3,40 @@
  */
 package codemining.lm.ngram.tui;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.SortedMap;
 import java.util.logging.Logger;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.DirectoryFileFilter;
 import org.apache.commons.lang.exception.ExceptionUtils;
 
-import codemining.java.codeutils.JavaTokenizer;
+import codemining.js.codeutils.JavaCodePrinter;
 import codemining.languagetools.ITokenizer;
-import codemining.languagetools.ITokenizer.FullToken;
 import codemining.lm.ngram.AbstractNGramLM;
 import codemining.lm.ngram.NGram;
 import codemining.util.serialization.ISerializationStrategy.SerializationException;
+
+import com.google.common.collect.Lists;
 
 /**
  * @author Miltos Allamanis <m.allamanis@ed.ac.uk>
  * 
  */
-public class HeatmapVizualizer {
+public class HeatmapVizualizer extends JavaCodePrinter {
 
 	private static final Logger LOGGER = Logger
 			.getLogger(HeatmapVizualizer.class.getName());
 
-	private static void addSlack(final String substring, final StringBuffer buf) {
-		for (char c : substring.toCharArray()) {
-			if (c == ' ') {
-				buf.append("&nbsp;");
-			} else if (c == '\n') {
-				buf.append("<br/>\n");
-			} else if (c == '\t') {
-				buf.append("&nbsp;&nbsp;&nbsp;");
-			} else {
-				buf.append(c);
-			}
-		}
-	}
-
-	/**
-	 * @param args
-	 * @throws IOException
-	 * @throws ClassNotFoundException
-	 * @throws SerializationException
-	 */
-	public static void main(String[] args) throws ClassNotFoundException,
-			SerializationException {
-		if (args.length != 3) {
-			System.err.println("Usage <fileDir> <lmModel> <tokenizerClass>");
-			return;
-		}
-
-		final AbstractNGramLM lm = AbstractNGramLM.readFromSerialized(args[1]);
-		for (File f : FileUtils.listFiles(new File(args[0]),
-				lm.modelledFilesFilter(), DirectoryFileFilter.DIRECTORY)) {
-			try {
-				writeHTMLwithXent(lm, f);
-			} catch (Exception e) {
-				LOGGER.warning(ExceptionUtils.getFullStackTrace(e));
-			}
-		}
-
-	}
-
-	/**
-	 * @param lm
-	 * @param codeFile
-	 * @throws IOException
-	 * @throws IllegalAccessException
-	 * @throws InstantiationException
-	 */
-	protected static void writeHTMLwithXent(final AbstractNGramLM lm,
-			final File codeFile) throws IOException, InstantiationException,
-			IllegalAccessException {
-		final String code = FileUtils.readFileToString(codeFile);
-		final File outFile = new File(codeFile.getName() + ".html");
-
-		final StringBuffer buf = new StringBuffer();
-
-		final ITokenizer jTokenizer = new JavaTokenizer(); // TODO: get from
-																// main
-		final SortedMap<Integer, FullToken> toks = jTokenizer
-				.fullTokenListWithPos(code.toCharArray());
-
+	private static List<ColoredToken> colorTokens(final AbstractNGramLM lm,
+			final File f) throws IOException {
 		final ITokenizer tokenizer = lm.getTokenizer();
-		final List<String> tokenList = tokenizer.tokenListFromCode(code
-				.toCharArray());
-
-		int i = 0;
-		int prevPos = 0;
-		buf.append("<html><body style='font-family:monospace; background-color:rgb(200,200,255)'>");
-		for (Entry<Integer, FullToken> entry : toks.entrySet()) {
-			if (i == 0 || entry.getKey() == Integer.MAX_VALUE) {
-				i++;
-				continue;
-			}
+		final List<String> tokenList = tokenizer.tokenListFromCode(FileUtils
+				.readFileToString(f).toCharArray());
+		final List<ColoredToken> coloredTokens = Lists.newArrayList();
+		for (int i = 0; i < tokenList.size(); i++) {
 			final NGram<String> ngram = NGram.constructNgramAt(i, tokenList,
 					lm.getN());
 			if (ngram.size() > 1) {
@@ -111,26 +47,50 @@ public class HeatmapVizualizer {
 					extraStyle = "text-decoration:underline; ";
 				}
 
-				addSlack(code.substring(prevPos, entry.getKey()), buf);
-				long hue = 0;
-				long saturation = 240;
-				long luminosity = 100 - Math
-						.round((100 * -Math.log(prob) / 20));
+				final float hue = 0;
+				final float saturation = 2.40f;
+				float luminosity = (float) (100. - Math.round((100 * -Math
+						.log(prob) / 20))) / 100;
 				if (luminosity < 0) {
 					luminosity = 0;
 				}
-				buf.append("<span style='background-color:hsl(" + hue + ","
-						+ saturation + "%," + luminosity + "%); color:hsl("
-						+ hue + "," + saturation + "%,"
-						+ ((luminosity + 50) % 100) + "%); " + extraStyle
-						+ "' data='" + prob + "'>" + entry.getValue().token
-						+ "</span>");
-			}
 
-			i++;
-			prevPos = entry.getKey() + entry.getValue().token.length();
+				coloredTokens.add(new ColoredToken(tokenList.get(i), Color
+						.getHSBColor(hue, saturation,
+								((luminosity + 50) % 100) / 100), Color
+						.getHSBColor(hue, saturation, luminosity), extraStyle));
+			} else {
+				coloredTokens.add(new ColoredToken(tokenList.get(i),
+						Color.BLACK));
+			}
 		}
-		buf.append("</body></html>");
-		FileUtils.writeStringToFile(outFile, buf.toString());
+
+		return coloredTokens;
+	}
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws SerializationException
+	 */
+	public static void main(final String[] args) throws ClassNotFoundException,
+			SerializationException {
+		if (args.length != 3) {
+			System.err.println("Usage <fileDir> <lmModel> <tokenizerClass>");
+			return;
+		}
+
+		final AbstractNGramLM lm = AbstractNGramLM.readFromSerialized(args[1]);
+		for (final File f : FileUtils.listFiles(new File(args[0]),
+				lm.modelledFilesFilter(), DirectoryFileFilter.DIRECTORY)) {
+			try {
+				final List<ColoredToken> coloredTokens = colorTokens(lm, f);
+				writeHTMLwithColors(coloredTokens, f);
+			} catch (final Exception e) {
+				LOGGER.warning(ExceptionUtils.getFullStackTrace(e));
+			}
+		}
+
 	}
 }
