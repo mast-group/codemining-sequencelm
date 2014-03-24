c2f04489fd9d9cb4fb621c54902bb44038a5e7a0
Miltos Allamanis <m.allamanis@ed.ac.uk>
2014-03-24 14:33:37 +0000

    Reverted Razvan's changes

--- src/main/java/codemining/lm/ngram/tui/NGramModelBuilder.java
+++ src/main/java/codemining/lm/ngram/tui/NGramModelBuilder.java
@@ -3,9 +3,7 @@
  */
 package codemining.lm.ngram.tui;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
@@ -28,79 +26,42 @@ public class NGramModelBuilder {
 	private static final Logger LOGGER = Logger
 			.getLogger(NGramModelBuilder.class.getName());
 
-	public static void main(String[] args) throws IOException,
+	public static void main(final String[] args) throws IOException,
 			ClassNotFoundException, InstantiationException,
 			IllegalAccessException, IllegalArgumentException,
 			InvocationTargetException, NoSuchMethodException,
 			SecurityException, SerializationException {
 
-
-		if (args.length != 5 && args.length != 4) {
+		if (args.length != 5) {
 			System.err
-					.println("Usage <TrainingFiles> <N> <NGramModel.ser output> <tokenizationClass> <WrapperSmootherClass> or <File w/ projects to use> <N> <tokenizationClass> <WrapperSmootherClass>" );
+					.println("Usage <TrainingFiles> <N> <NGramModel.ser output> <tokenizationClass> <WrapperSmootherClass>");
 			return;
 		}
-		
-		if(args.length == 4){
-			BufferedReader br = null;
-			try {
-	 			String sCurrentLine;
-	 			br = new BufferedReader(new FileReader(args[0]));
-	 
-				while ((sCurrentLine = br.readLine()) != null) {
-					if (sCurrentLine.trim().length() > 0) {
-						String proj = sCurrentLine.trim().split(" ")[1];
-						//System.out.println(proj);
-						String input = "/afs/inf.ed.ac.uk/group/ML/s0954584/java_projects/train/" + proj;
-						String output = "/afs/inf.ed.ac.uk/group/ML/s0954584/java_projects/projModels/" + proj + ".filtered.laplace.ser";
-						buildModel(input,args[1],output,args[2],args[3]);
-						//System.exit(3);
-					}
-				}
-	 
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (br != null)
-						br.close();
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
-			}
-		}
 
-		if (args.length == 5) {
-			buildModel(args[0],args[1],args[2],args[3],args[4]);
-		}
-	}
-	
-	private static void buildModel(String sInput, String sN, String sOutput, String sTokenizer, String sSmoother) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IOException, IllegalArgumentException, SecurityException, InvocationTargetException, NoSuchMethodException, SerializationException{
-		final ITokenizer tokenizer;
-		
 		final Class<? extends ITokenizer> tokenizerName = (Class<? extends ITokenizer>) Class
-				.forName(sTokenizer);
-		tokenizer = tokenizerName.newInstance();
+				.forName(args[3]);
+		final ITokenizer tokenizer = tokenizerName.newInstance();
 
-		final NGramLM dict = new NGramLM(Integer.parseInt(sN), tokenizer);
+		final NGramLM dict = new NGramLM(Integer.parseInt(args[1]), tokenizer);
 
-		LOGGER.info("NGram Model creater started with " + sN
-				+ "-gram for files in " + sInput + " using " + sTokenizer
+		LOGGER.info("NGram Model creater started with " + args[1]
+				+ "-gram for files in " + args[0] + " using " + args[3]
 				+ " tokenizer");
 
-		final Collection<File> files = FileUtils.listFiles(new File(sInput),
+		final Collection<File> files = FileUtils.listFiles(new File(args[0]),
 				dict.modelledFilesFilter(), DirectoryFileFilter.DIRECTORY);
-		
+
 		dict.trainModel(files);
 
 		LOGGER.info("Ngram model build. Adding Smoother...");
 		final Class<? extends AbstractNGramLM> smoothedNgramClass = (Class<? extends AbstractNGramLM>) Class
-				.forName(sSmoother);
+				.forName(args[4]);
 		final AbstractNGramLM ng = (AbstractNGramLM) smoothedNgramClass
 				.getDeclaredConstructor(AbstractNGramLM.class)
 				.newInstance(dict);
 
 		LOGGER.info("Ngram model build. Serializing...");
-		ng.serializeToDisk(sOutput);
+		ng.serializeToDisk(args[2]);
+
 	}
 }
